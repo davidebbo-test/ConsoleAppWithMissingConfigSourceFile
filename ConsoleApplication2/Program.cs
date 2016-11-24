ab0850339b5096b1870c3dc2785c4be5887a51a0
David Ebbo <david.ebbo@gmail.com>
2016-11-24 09:21:02 -0800

    Added comments

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -6,7 +6,10 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+            // This line works fine because config system ignores missing 'appSettings file='
             string foo = ConfigurationManager.AppSettings["foo"];
+
+            // This line blows up because config system doesn't allow missing 'connectionStrings configSource='
             ConnectionStringSettings bar = ConfigurationManager.ConnectionStrings["bar"];
         }
     }
